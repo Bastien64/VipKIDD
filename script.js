1c7d5a52b60ca4e8f48f3cff3ef871186efb26c2
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-10-26 17:04:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const textdiv3 = document.querySelector(".textdiv3");
+    
+    const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+            textdiv3.classList.add("show");
+            observer.disconnect(); // Arrête l'observation une fois l'animation déclenchée.
+        }
+    });
+
+    observer.observe(textdiv3);
+});
+
+
