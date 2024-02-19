24aca1f0f12a76fe3e1fe1c7de65498c652cacf6
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-02-19 22:22:53 +0100

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -12,3 +12,58 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+$(document).ready(function() {
+    // Fonction pour vérifier si l'élément est visible dans la fenêtre
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+    // Fonction pour gérer le défilement
+    function handleScroll() {
+      var logo = $(".logovipkiddiv2");
+      if (isElementInViewport(logo[0])) {
+        logo.addClass("show-logo");
+      }
+    }
+
+    // Attacher la fonction handleScroll à l'événement de défilement
+    $(window).on("scroll", handleScroll);
+
+    // Appeler handleScroll au chargement initial de la page
+    handleScroll();
+  });
+
+  $(document).ready(function() {
+    // Fonction pour vérifier si l'élément est visible dans la fenêtre
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+    // Fonction pour gérer le défilement
+    function handleScroll() {
+      var images = $(".imagebandeau");
+      images.each(function(index, element) {
+        if (isElementInViewport(element)) {
+          $(element).addClass("show-image");
+        }
+      });
+    }
+
+    // Attacher la fonction handleScroll à l'événement de défilement
+    $(window).on("scroll", handleScroll);
+
+    // Appeler handleScroll au chargement initial de la page
+    handleScroll();
+  });
